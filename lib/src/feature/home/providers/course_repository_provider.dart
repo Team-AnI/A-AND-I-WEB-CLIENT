9b1bff28bb453a7c8040ab3ae3c3f1af4bb5c866
sangwook16074 <qazws78941@gmail.com>
2026-04-17 22:21:24 +0900

    fix: refresh expired v2 access tokens

--- lib/src/feature/home/providers/course_repository_provider.dart
+++ lib/src/feature/home/providers/course_repository_provider.dart
@@ -1,7 +1,6 @@
-import 'package:a_and_i_report_web_server/src/core/constants/api_url.dart';
+import 'package:a_and_i_report_web_server/src/core/providers/package_api_client_providers.dart';
 import 'package:a_and_i_report_web_server/src/feature/home/data/repositories/course_repository_adapter.dart';
 import 'package:a_and_i_report_web_server/src/feature/home/data/repositories/course_repository.dart';
-import 'package:aandi_course_api/aandi_course_api.dart' as course_api;
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -10,8 +9,6 @@ part 'course_repository_provider.g.dart';
 @riverpod
 CourseRepository courseRepository(Ref ref) {
   return CourseRepositoryAdapter(
-    client: course_api.CourseApiClient(
-      baseUrl: baseUrl,
-    ),
+    client: ref.read(courseApiClientProvider),
   );
 }
--- lib/src/feature/home/providers/report_summary_repository_provider.dart
+++ lib/src/feature/home/providers/report_summary_repository_provider.dart
@@ -1,7 +1,6 @@
 import 'package:a_and_i_report_web_server/src/feature/home/data/repositories/report_summary_repository.dart';
 import 'package:a_and_i_report_web_server/src/feature/home/data/repositories/report_summary_repository_adapter.dart';
-import 'package:a_and_i_report_web_server/src/core/constants/api_url.dart';
-import 'package:aandi_course_api/aandi_course_api.dart' as course_api;
+import 'package:a_and_i_report_web_server/src/core/providers/package_api_client_providers.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -10,8 +9,6 @@ part 'report_summary_repository_provider.g.dart';
 @riverpod
 ReportSummaryRepository reportSummaryRepository(Ref ref) {
   return ReportSummaryRepositoryAdapter(
-    client: course_api.CourseApiClient(
-      baseUrl: baseUrl,
-    ),
+    client: ref.read(courseApiClientProvider),
   );
 }
